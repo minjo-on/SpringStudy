5d1804e95cbf512c56ff4edf867621a7b8a9c0de
minjo-on <103498687+minjo-on@users.noreply.github.com>
2024-01-09 16:53:28 +0900

    validation message
    messages properties 사용해보기

--- demo/src/main/java/com/example/demo/form/CalcForm.java
+++ demo/src/main/java/com/example/demo/form/CalcForm.java
@@ -6,12 +6,12 @@ import org.hibernate.validator.constraints.Range;
 
 @Data
 public class CalcForm {
-    @NotNull(message = "왼쪽 : 숫자를 입력해주세요")
-    @Range(min = 1,max = 10,message = "왼쪽 : {min}~{max} 범위의 숫자를 입력해주세요.")
+    @NotNull
+    @Range(min = 1,max = 10)
     private Integer leftNum;
 
-    @NotNull(message = "오른쪽 : 숫자를 입력해주세요")
-    @Range(min = 1,max = 10,message = "오른쪽 : {min}~{max} 범위의 숫자를 입력해주세요.")
+    @NotNull
+    @Range(min = 1,max = 10)
     private Integer rightNum;
 
 }

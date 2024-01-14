9296c9861a6fcd0aba96d3af5ad83ebadb1ae8b8
minjo-on <103498687+minjo-on@users.noreply.github.com>
2024-01-15 00:24:37 +0900

    165p aop

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,20 +1,26 @@
 package com.example.demo;
 
+import chap07.Calculator;
 import chap07.ExeTimeCalculator;
 import chap07.ImpeCalculator;
 import chap07.RecCalculator;
+import config.AppCtx;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 	public static void main(String[] args) {
 //		SpringApplication.run(DemoApplication.class, args);
-		ExeTimeCalculator ttCal1 = new ExeTimeCalculator(new ImpeCalculator());
-		System.out.println(ttCal1.factorial(20));
 
-		ExeTimeCalculator ttCal2 = new ExeTimeCalculator(new RecCalculator());
-		System.out.println(ttCal2.factorial(20));
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppCtx.class);
+
+		Calculator cal = ctx.getBean("calculator",Calculator.class);
+		long fiveFact = cal.factorial(5);
+		System.out.println(fiveFact);
+		System.out.println(cal.getClass().getName());
+		ctx.close();
 	}
 
 }

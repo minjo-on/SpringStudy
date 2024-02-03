14e1eb1cedb9c8080f43069667201fcfb9a18e0d
Minjoon Park <103498687+minjo-on@users.noreply.github.com>
2024-02-03 18:28:07 +0900

    Add files via upload

---
+++ study/src/main/java/com/example/study/entity/DeliveryStatus.java
@@ -0,0 +1,5 @@
+package com.example.study.entity;
+
+public enum DeliveryStatus {
+    READY, COMP
+}
---
+++ study/src/main/java/com/example/study/entity/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.example.study.entity;
+
+public enum OrderStatus {
+    ORDER,CANCEL
+}
